aa0be642b0ee52e4b3baf9d484cf0e0bc0559c3a
josh muleshi <jmuleshi2@gmail.com>
2021-07-11 11:22:59 +0100

    added a navGraph with all functionality that come with it and need in this project

--- app/src/main/java/com/example/eventcademyui/MainActivity.kt
+++ app/src/main/java/com/example/eventcademyui/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.eventcademyui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     lateinit var recyclerView: RecyclerView
@@ -12,9 +15,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recyclerView = findViewById(R.id.recyclerView)
-        recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+//        recyclerView = findViewById(R.id.recyclerView)
+//        recyclerView.setHasFixedSize(true)
+//        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        val bottomNavigationView:BottomNavigationView = findViewById(R.id.bottom_navigation)
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        bottomNavigationView.setupWithNavController(navController)
 
     }
 }
