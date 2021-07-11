3a0019c3684b6349b5e37ceded09a0392e64d6cf
josh muleshi <jmuleshi2@gmail.com>
2021-07-11 12:41:12 +0100

    added the data and favorite btn on the cardView or the recycleView item

--- app/src/main/java/com/example/eventcademyui/FavoriteFragment.kt
+++ app/src/main/java/com/example/eventcademyui/FavoriteFragment.kt
@@ -16,7 +16,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [FavoriteFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SettingsFragment : Fragment() {
+class FavoriteFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
     }
 
     companion object {
