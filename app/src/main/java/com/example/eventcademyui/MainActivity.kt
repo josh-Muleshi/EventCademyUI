00e1925f41e56e0663b287a9a0887ba836d25ac9
josh muleshi <jmuleshi2@gmail.com>
2021-07-11 08:38:11 +0100

    the recycleView ui and the bottom bar well added

--- app/src/main/java/com/example/eventcademyui/MainActivity.kt
+++ app/src/main/java/com/example/eventcademyui/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.eventcademyui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
     }
 }
