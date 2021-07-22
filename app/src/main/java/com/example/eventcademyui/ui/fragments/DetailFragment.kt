af3de3f228a8528f25d9ff8ef376fe57292e1a79
josh muleshi <jmuleshi2@gmail.com>
2021-07-22 11:39:44 +0100

    added an other fragment call fragment_detail.xml

---
+++ app/src/main/java/com/example/eventcademyui/ui/fragments/DetailFragment.kt
@@ -0,0 +1,22 @@
+package com.example.eventcademyui.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.eventcademyui.R
+import com.example.eventcademyui.databinding.FragmentDetailBinding
+
+class detailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentDetailBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
+        return binding.root
+    }
+}
